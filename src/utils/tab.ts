0f271825c20b2d3a8b8716d4c3caa7d310884535
Haotian Yi <yi2021@g.ucla.edu>
2025-05-12 15:43:49 -0700

    Add formTabName utility and integrate tab name updates for all views

---
+++ src/utils/tab.ts
@@ -0,0 +1,8 @@
+/**
+ * Forms the tab name for the browser from a label
+ * @param label workspace label, component name, etc.
+ * @returns the formatted tab name
+ */
+export function formTabName(label: string): string {
+  return `${label} - Ownly`
+}
